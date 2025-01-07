4f9f7474cdf353489f6ce9f0447f36e663ed5ff8
Vikash Khati <vikashkhati007@gmail.com>
2025-01-07 11:47:41 +0530

    fix 2.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true
+    }
+  }
+  module.exports = nextConfig
+  
